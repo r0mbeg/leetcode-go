36cca6a019814c3f89228aa161f7851e94d4850f
Roman <romaproffen@yandex.ru>
2026-04-01 13:59:39 +0300

    solve: p0349 O(n)

--- problems/p0349_two_arrays_intersect/solution.go
+++ problems/p0349_two_arrays_intersect/solution.go
@@ -3,26 +3,16 @@ package p0349_two_arrays_intersect
 func intersection(nums1 []int, nums2 []int) []int {
 	var res []int
 
-	if len(nums1) == 0 || len(nums2) == 0 {
-		return res
+	set := make(map[int]struct{})
+
+	for _, num := range nums1 {
+		set[num] = struct{}{}
 	}
 
-	countMap1 := make(map[int]int)
-
-	for i := range nums1 {
-		//fmt.Printf("nums1[%d]=%d added to map \n", i, nums1[i])
-		countMap1[nums1[i]]++
-	}
-
-	countMap2 := make(map[int]int)
-
-	for i := range nums2 {
-		countMap2[nums2[i]]++
-	}
-
-	for k1, _ := range countMap1 {
-		if _, ok := countMap2[k1]; ok {
-			res = append(res, k1)
+	for _, num := range nums2 {
+		if _, ok := set[num]; ok {
+			res = append(res, num)
+			delete(set, num)
 		}
 	}
 
